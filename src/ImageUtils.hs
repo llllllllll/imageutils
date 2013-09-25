36a98e6e3b17454b283e3d043f8184321ed699ce
llllllllll <joejev@gmail.com>
2013-09-24 22:56:53 -0400

    added drawing functions to be used in the turtle

--- src/ImageUtils.hs
+++ src/ImageUtils.hs
@@ -15,20 +15,31 @@
 module ImageUtils 
     ( module Data.Array.Unboxed
     , module Codec.Image.DevIL
+    , PrimaryColor(..)
+    , Color
     , Image
-    , Color(..)
-    -- * Color changes
-    , strip           -- :: Color -> Image -> Image
-    , (<\\>)          -- :: Image -> Color -> Image
-    , filter_img      -- :: Color -> Image -> Image
-    , (<#>)           -- :: Image -> Color -> Image
+    -- * Colors
+    , rED
+    , gREEN
+    , bLUE
+    , cYAN
+    , bLACK
+    , wHITE
+    -- * Primary color changes
+    , strip           -- :: PrimaryColor -> Image -> Image
+    , (<\\>)          -- :: Image -> PrimaryColor -> Image
+    , filter_img      -- :: PrimaryColor -> Image -> Image
+    , (<#>)           -- :: Image -> PrimaryColor -> Image
     , edit_brightness -- :: Double -> Image
-    , edit_color      -- :: Color -> Double -> Image
-    , invert_colors   -- :: Image -> Image
-    , invert_color    -- :: Color -> Image
+    , edit_primary      -- :: PrimaryColor -> Double -> Image
+    , invert_primarys   -- :: Image -> Image
+    , invert_primary    -- :: PrimaryColor -> Image
     -- * Orientation
     , flip_vert       -- :: Image -> Image
     , flip_horz       -- :: Image -> Image
+    -- * Drawing
+    , draw_seg        -- :: (Int,Int) -> (Int,Int) -> Color -> Image -> Image
+    , draw_ln         -- :: (Int,Int) -> (Int,Int) -> Color -> Image -> Image
     -- * Other
     , merge           -- :: Image -> Image -> Image
     ) where
@@ -36,55 +47,79 @@ module ImageUtils
 import Codec.Image.DevIL
 import Data.Array.Unboxed
 
+-- |Data type for primary colors
+data PrimaryColor = Red | Green | Blue
+
+-- |The color channels on in the RGB images.
+type Color = (Word8,Word8,Word8)
+
 -- |Alias for the type of data returned from readImage.
 type Image = UArray (Int,Int,Int) Word8 -- Indices: (R,G,B)
 
--- |The color channels on in the RGB images.
-data Color = Red | Green | Blue | Alpha
+-- |Red color triplet.
+rED   :: Color
+rED   = (255,0,0)
+
+-- |Green color triplet.
+gREEN :: Color
+gREEN = (0,255,0)
+
+-- |Blue color triplet.
+bLUE  :: Color
+bLUE  = (0,0,255)
+
+-- |Cyan color triplet.
+cYAN :: Color
+cYAN = (0,255,255)
+
+-- |Black color triplet.
+bLACK :: Color
+bLACK = (0,0,0)
+
+-- |White color triplet.
+wHITE :: Color
+wHITE = (255,255,255)
 
 
 -- Testing functions.
 main :: IO ()
 main = do
     ilInit
-    
-
+    let img = listArray ((0,0,0),(499,499,3)) (repeat 255)
+    writeImage "../testln2.jpg" (draw_seg (24,40) (50,25) (0,0,0) img)
 
 -- -----------------------------------------------------------------------------
--- Color stripping
+-- PrimaryColor stripping
 
 -- |Strips an Image of all of a primary color.
 
-strip :: Color -> Image -> Image
+strip :: PrimaryColor -> Image -> Image
 strip Red   img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x == 0) i]
 strip Green img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x == 1) i]
 strip Blue  img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x == 2) i]
-strip Alpha img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x == 3) i]
-
 
 -- |An alias of strip.
 -- 
 -- > img <\\> color == strip color img
-(<\\>):: Image -> Color -> Image
+(<\\>):: Image -> PrimaryColor -> Image
 (<\\>) img color = strip color img
 
 -- -----------------------------------------------------------------------------
--- Color Filtering
+-- PrimaryColor Filtering
 
-filter_img :: Color -> Image -> Image
+filter_img :: PrimaryColor -> Image -> Image
 filter_img Red   img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x /= 0) i]
 filter_img Green img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x /= 1) i]
 filter_img Blue  img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x /= 2) i]
-filter_img Alpha img = img//[(i,0) | i <- indices img, (\(_,_,x) -> x /= 3) i]
 
 -- |An alias of filter_img
 -- 
 -- > img <#> color == filter_img color img
-(<#>) :: Image -> Color -> Image
+(<#>) :: Image -> PrimaryColor -> Image
 (<#>) img color = filter_img color img
 
 -- -----------------------------------------------------------------------------
--- Color Editing 
+-- PrimaryColor Editing 
 
 -- |Edits the value of all colors by multiplying the values by n.
 -- 
@@ -100,58 +135,49 @@ edit_brightness n img = img//[(i,edit_brightness' n (img!i)) | i <- indices img]
 
 -- |Edits the value of the given primary color by multiplying the values by n.
 -- 
--- > edit_color Red   0 img == strip Red
--- > edit_color Green 0 img == strip Green
--- > edit_color Blue  0 img == strip Blue
--- > edit_color _     1 img == img
-edit_color :: Color -> Double -> Image -> Image
-edit_color _ 1 img = img
-edit_color Red   n img = img//[(i,edit_red n (img!i)) | i <- indices img,
-                               (\(_,_,x) -> x == 0) i]
+-- > edit_primary Red   0 img == strip Red
+-- > edit_primary Green 0 img == strip Green
+-- > edit_primary Blue  0 img == strip Blue
+-- > edit_primary _     1 img == img
+edit_primary :: PrimaryColor -> Double -> Image -> Image
+edit_primary _ 1 img = img
+edit_primary Red   n img = img//[(i,edit_red n (img!i)) | i <- indices img,
+                                 (\(_,_,x) -> x == 0) i]
   where
       edit_red n m
           | floor (n * fromIntegral m) > 255 = 255
           | otherwise = floor $ n * fromIntegral m
-edit_color Green n img = img//[(i,edit_green n (img!i)) | i <- indices img,
-                               (\(_,_,x) -> x == 1) i]
+edit_primary Green n img = img//[(i,edit_green n (img!i)) | i <- indices img,
+                                 (\(_,_,x) -> x == 1) i]
   where
       edit_green n m
           | floor (n * fromIntegral m) > 255 = 255
           | otherwise = floor $ n * fromIntegral m
-edit_color Blue  n img = img//[(i,edit_blue n (img!i)) | i <- indices img,
-                               (\(_,_,x) -> x == 2) i]
+edit_primary Blue  n img = img//[(i,edit_blue n (img!i)) | i <- indices img,
+                                 (\(_,_,x) -> x == 2) i]
   where
       edit_blue n m
-          | floor (n * fromIntegral m) > 255 = 255
-          | otherwise = floor $ n * fromIntegral m
-edit_color Alpha n img = img//[(i,edit_alpha n (img!i)) | i <- indices img,
-                               (\(_,_,x) -> x == 3) i]
-  where
-      edit_alpha n m
           | floor (n * fromIntegral m) > 255 = 255
           | otherwise = floor $ n * fromIntegral m
 
 -- |Inverts the colors of the Image.
 -- 
--- @ invert_colors == invert_color Red $ invert_color Green 
---                  $ invert_color Blue img @
-invert_colors :: Image -> Image
-invert_colors img = amap (255-) img
+-- @ invert_primarys == invert_primary Red $ invert_primary Green 
+--                  $ invert_primary Blue img @
+invert_primarys :: Image -> Image
+invert_primarys img = amap (255-) img
 
 -- |Inverts the values of the color in the image.
 -- 
--- @ invert_colors == invert_color Red $ invert_color Green 
---                  $ invert_color Blue img @
-invert_color :: Color -> Image -> Image
-invert_color Red   img = img//[(i,255-img!i) | i <- indices img, 
-                                                    (\(_,_,c) -> c) i == 0]
-invert_color Green img = img//[(i,255-img!i) | i <- indices img, 
-                                                    (\(_,_,c) -> c) i == 1]
-invert_color Blue  img = img//[(i,255-img!i) | i <- indices img, 
-                                                    (\(_,_,c) -> c) i == 2]
-invert_color Alpha img = img//[(i,255-img!i) | i <- indices img, 
-                                                    (\(_,_,c) -> c) i == 3]
-
+-- @ invert_primarys == invert_primary Red $ invert_primary Green 
+--                  $ invert_primary Blue img @
+invert_primary :: PrimaryColor -> Image -> Image
+invert_primary Red   img = img//[(i,255-img!i) | i <- indices img, 
+                                                      (\(_,_,c) -> c) i == 0]
+invert_primary Green img = img//[(i,255-img!i) | i <- indices img, 
+                                                      (\(_,_,c) -> c) i == 1]
+invert_primary Blue  img = img//[(i,255-img!i) | i <- indices img, 
+                                                      (\(_,_,c) -> c) i == 2]
 -- -----------------------------------------------------------------------------
 -- Orientation
 
@@ -164,6 +190,40 @@ flip_vert img = let m = (\(r,_,_) -> r) $ (snd . bounds) img
 flip_horz :: Image -> Image
 flip_horz img = let m = (\(_,c,_) -> c) $ (snd . bounds) img
                 in ixmap (bounds img) (\(r,c,v) -> (r,m-c,v)) img
+
+-- -----------------------------------------------------------------------------
+-- Drawing
+
+-- |Draws a straight line segment from (x,y) to (x',y') in color c
+draw_seg :: (Int,Int) -> (Int,Int) -> Color -> Image -> Image
+draw_seg (x,y) (x',y') c img 
+    | x == x'   = img//[((m,x,v),val v c) | m <- [min y y'..max y y']
+                       , v <- [0..2]]
+    | otherwise = let sl = fromIntegral (y' - y) / fromIntegral (x' - x)
+                  in img//[((m,n,v),val v c) | n <- [min x x'..max x x']
+                          , let m = round (sl * fromIntegral (n - x)) + y
+                          , v <- [0..2]]
+  where
+      val 0 (r,_,_) = r
+      val 1 (_,g,_) = g
+      val 2 (_,_,b) = b
+
+-- |Draws a line that intersects (x,y) and (x',y') in color c
+-- MUST FIX FOR NON HORIZONTAL OR VERTICAL!
+draw_ln :: (Int,Int) -> (Int,Int) -> Color -> Image -> Image
+draw_ln (x,y) (x',y') c img
+    | x == x'   = img//[((m,x,v),val v c) | m <- [0..(\(r,_,_) -> r) 
+                                                         ((snd . bounds) img)]
+                       , v <- [0..2]]
+    | otherwise = let sl    = fromIntegral (y' - y) / fromIntegral (x' - x)
+                      max_x = (\(_,c,_) -> c) ((snd . bounds) img)
+                  in img//[((m,n,v),val v c) | n <- [0..max_x]
+                          , let m = round (sl * fromIntegral (n - x)) + y
+                          , v <- [0..2]]
+  where
+      val 0 (r,_,_) = r
+      val 1 (_,g,_) = g
+      val 2 (_,_,b) = b
 
 -- -----------------------------------------------------------------------------
 -- Other --
--- src/turtle.hs
+++ src/turtle.hs
@@ -18,8 +18,8 @@ import ImageUtils
 
 -- |Data type for the turtle's state.
 data Turtle = Turtle { loc   :: (Int,Int) -- Turtles location on the image.
-                     , color :: Int       -- The color channel of the tail.
-                     , moves :: Int       -- The amount of moves (errors).
+                     , color :: Color     -- The color of the tail.
+                     , comln :: Int       -- The amount of commands (errors).
                      , image :: Image     -- The image (default is 500,500).
                      }
 
@@ -27,9 +27,9 @@ data Turtle = Turtle { loc   :: (Int,Int) -- Turtles location on the image.
 nEW_TURTLE :: IO Turtle
 nEW_TURTLE = return 
              $ Turtle { loc   = (0,0)
-                      , color = 0
+                      , color = bLACK
                       , image = listArray ((0,0,0),(499,499,3)) (repeat 255)
-                      , moves = 0
+                      , comln = 1
                       }
 
 -- |Initializes devIL and starts with a new turtle.
@@ -46,94 +46,76 @@ recurs iot = do
 -- |Parse a line of user input or file input.
 parse_ln :: [String] -> Turtle -> IO Turtle
 parse_ln cs t
-    | head cs == "move"  = (move t (cs!!1) (read $ cs!!2)) 
+    | null cs           = return $ t { comln = comln t + 1 } -- Empty lines.
+    | head cs == "move"  = (move t (cs!!1) (read $ cs!!2))
+                           >>= (\t' -> return $ t' { comln = comln t + 1 })
     | head cs == "color" = color_change t (cs!!1)
+                           >>= (\t' -> return $ t' { comln = comln t + 1 })
     | head cs == "write" = write t (cs!!1)
     | head cs == "start" = return 
                            $ Turtle { loc   = (read (cs!!2),read (cs!!1))
-                                    , color = 0
+                                    , color = bLACK
                                     , image = listArray ((0,0,0),(499,499,3)) 
                                               (repeat 255)
-                                    , moves = 0
+                                    , comln = comln t + 1
                                     }
-    | otherwise = error ("Parse error after " ++ show (moves t) ++ " moves")
+    | head cs == "--"   = return $ t { comln = comln t + 1 } -- Comments.
+    | otherwise = error ("Parse error on line " ++ show (comln t) 
+                         ++ ": exiting!")
                   >> exitSuccess >> nEW_TURTLE
 
 -- |Moves the turtle leaving a trail behind it.
 move :: Turtle -> String -> Int -> IO Turtle
 move t dir n
     | dir == "up"    = return 
-                       $ Turtle { loc   = let (x,y) = loc t in (x,y+n)
-                                , color = color t
-                                , moves = moves t + 1
-                                , image = move_up t n
-                                }
+                       $ t { loc   = let (x,y) = loc t in (x,y+n)
+                           , image = move_up t n
+                           }
     | dir == "down"  = return 
-                       $ Turtle { loc   = let (x,y) = loc t in (x,y-n)
-                                , color = color t
-                                , moves = moves t + 1
-                                , image = move_down t n
-                                }
+                       $ t { loc   = let (x,y) = loc t in (x,y-n)
+                           , image = move_down t n
+                           }
     | dir == "left"  = return 
-                       $ Turtle { loc   = let (x,y) = loc t in (x-n,y)
-                                , color = color t
-                                , moves = moves t + 1
-                                , image = move_left t n
-                                }
+                       $ t { loc   = let (x,y) = loc t in (x-n,y)
+                           , image = move_left t n
+                           }
     | dir == "right" = return 
-                       $ Turtle { loc   = let (x,y) = loc t in (x+n,y)
-                                , color = color t
-                                , moves = moves t + 1
-                                , image = move_right t n
-                                }
+                       $ t { loc   = let (x,y) = loc t in (x+n,y)
+                           , image = move_right t n
+                           }
 
 -- |Draws the move up data to the image.
 move_up :: Turtle -> Int -> Image
 move_up t n = let (x,y) = loc t
-              in (image t)//[(i,if (\(_,_,c) -> c) i == color t
-                                  then 255
-                                  else 0)
-                             | i <- filter (\(y',x',_)-> y' `elem` [y..y+n]
-                                                         && x' == x)
-                                    (indices (image t))]
+                  img  = image t
+              in draw_seg (x,y) (x,y+n) (color t) img   
 
 -- |Draws the move down data to the image.
 move_down :: Turtle -> Int -> Image
-move_down t n = let (x,y) = loc t 
-                in (image t)//[(i,if (\(_,_,c) -> c) i == color t
-                                  then 255
-                                  else 0)
-                               | i <- filter (\(y',x',_) -> y' `elem` [y-n..y]
-                                                            && x' == x)
-                                      (indices (image t))]
+move_down t n = let (x,y) = loc t
+                    img  = image t
+                in draw_seg (x,y) (x,y-n) (color t) img  
 
 -- |Draws the move left data to the image.
 move_left :: Turtle -> Int -> Image
-move_left t n = let (x,y) = loc t 
-                in (image t)//[(i,if (\(_,_,c) -> c) i == color t
-                                  then 255
-                                  else 0)
-                               | i <- filter (\(y',x',_) -> x' `elem` [x-n..x]
-                                                            && y' == y)
-                                      (indices (image t))]
+move_left t n = let (x,y) = loc t
+                    img  = image t
+                in draw_seg (x,y) (x-n,y) (color t) img  
 
 -- |Draws the move right data to the image.
 move_right :: Turtle -> Int -> Image
-move_right t n = let (x,y) = loc t 
-                 in (image t)//[(i,if (\(_,_,c) -> c) i == color t
-                                  then 255
-                                  else 0) 
-                                | i <- filter (\(y',x',_) -> x' `elem` [x..x+n]
-                                                             && y' == y)
-                                       (indices (image t))]
+move_right t n = let (x,y) = loc t
+                     img  = image t
+                 in draw_seg (x,y) (x+n,y) (color t) img  
 
 -- |Changes the color channel of the tail of the turtle.
 color_change :: Turtle -> String -> IO Turtle
-color_change t "red"   = return t { color = 0 }
-color_change t "green" = return t { color = 1 }
-color_change t "blue"  = return t { color = 2 }
-color_change t "alpha" = return t { color = 3 }
-color_change t _       = error "Not a valid color" >> return t
+color_change t "red"   = return t { color = rED      }
+color_change t "green" = return t { color = gREEN    }
+color_change t "blue"  = return t { color = bLUE     }
+color_change t "black" = return t { color = bLACK    }
+color_change t "white" = return t { color = wHITE    }
+color_change t str     = return t { color = read str }
 
 -- |Write's the turtles image to a file named fl.
 write :: Turtle -> FilePath -> IO Turtle
